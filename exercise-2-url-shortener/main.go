cc5b0a07e4d72e2b04629877b31d7a4235b6da0a
Brayden Di Luca <braydendiluca@gmail.com>
2021-07-13 12:20:38 +1000

    Update implementation to read yaml from file

--- exercise-2-url-shortener/main.go
+++ exercise-2-url-shortener/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	flags := setupFlags()
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -17,18 +22,41 @@ func main() {
 
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := YAMLHandler([]byte(yaml), mapHandler)
+	yamlHandler, err := YAMLHandler(getPathsFromYaml(flags.yamlFile), mapHandler)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
+}
+
+type Flags struct {
+	yamlFile string
+}
+
+func setupFlags() Flags {
+	var yamlFile string
+	flag.StringVar(&yamlFile, "yaml", "default.yml", "Yaml file to read path/url pair from")
+
+	flag.Parse()
+
+	return Flags{yamlFile}
+}
+
+func getPathsFromYaml(filename string) []byte{
+	file, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	yamlBytes, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return yamlBytes
 }
 
 func defaultMux() *http.ServeMux {
