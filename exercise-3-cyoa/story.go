9b041689bf36d6cd3fdd9fe6759eb49dd6797b0b
Brayden Di Luca <braydendiluca@gmail.com>
2021-07-13 18:12:06 +1000

    Add methods for parsing and templating stories

--- exercise-3-cyoa/story.go
+++ exercise-3-cyoa/story.go
@@ -1,3 +1,38 @@
 package main
 
-type story struct {}
+import (
+	"encoding/json"
+	"html/template"
+	"log"
+)
+
+type story struct {
+	Title string `json:"title"`
+	Story []string `json:"story"`
+	Options []option `json:"options"`
+}
+
+type option struct {
+	Text string `json:"text"`
+	Arc string `json:"arc"`
+}
+
+func parseStories(storyBytes []byte) (stories map[string]story){
+	err := json.Unmarshal(storyBytes, &stories)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return stories
+}
+
+func buildTemplateForStory() *template.Template{
+	htmlTemplate, err := template.New("StoryTemplate").Parse(string(parseFile("template.html")))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return htmlTemplate
+}
