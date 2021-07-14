945b83f4b9bdaac2e69392b74b1fa8e75124e8e4
Brayden Di Luca <braydendiluca@gmail.com>
2021-07-14 16:41:56 +1000

    Add htmlfile flag to allow parsing any html file

--- exercise-4-link-parser/main.go
+++ exercise-4-link-parser/main.go
@@ -1,7 +1,24 @@
 package main
 
+import "flag"
+
+type Flags struct{
+	htmlFilename string
+}
+
+func setupFlags() Flags {
+	var htmlFilename string
+	flag.StringVar(&htmlFilename, "htmlfile", "example1.html", "The HTML file you wish to extract the links from")
+	flag.Parse()
+
+	return Flags{
+		htmlFilename,
+	}
+}
+
 func main() {
-	links := GetLinksFromHtml(parseHtmlFile("example1.html"))
+	flags := setupFlags()
+	links := GetLinksFromHtml(parseHtmlFile(flags.htmlFilename))
 
 	printLinks(links)
 }
